9255e25c4adeaa358580b83490627804e1702fe9
William Loosman <william.loosman@onegini.com>
2018-07-06 15:09:37 +0200

    First working example
    Signed-off-by: William Loosman <william.loosman@onegini.com>

--- DotnetAspCoreMvcExample/Startup.cs
+++ DotnetAspCoreMvcExample/Startup.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
@@ -10,8 +13,10 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using DotnetAspCoreMvcExample.Data;
+using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Linq;
 
 namespace DotnetAspCoreMvcExample
 {
@@ -39,6 +44,28 @@ namespace DotnetAspCoreMvcExample
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddAuthentication().AddOnegini(o =>
+            {
+                o.ClientId = "8EC5395C7AD02E64AAAA88BAF2C5B34423A3D144F2008BA1F0C09859BBE4DAD9";
+                o.ClientSecret = "065E8CB4F3573145189EA02739E642C5AE1BADEE3150078CAEB90B4F6DFC321D";
+                o.AuthorizationEndpoint = "https://onegini-msp-snapshot.test.onegini.io/oauth/authorize";
+                o.TokenEndpoint = "https://onegini-msp-snapshot.test.onegini.io/oauth/token";
+                o.UserInformationEndpoint = "https://onegini-msp-snapshot.test.onegini.io/client/resource/profile";
+            });
+
+            /*
+            services.AddAuthentication().AddOAuth("Onegini", c =>
+            {
+                c.ClientId = "8EC5395C7AD02E64AAAA88BAF2C5B34423A3D144F2008BA1F0C09859BBE4DAD9";
+                c.ClientSecret = "065E8CB4F3573145189EA02739E642C5AE1BADEE3150078CAEB90B4F6DFC321D";
+                c.Scope.Add("read");
+                c.Scope.Add("profile");
+                c.CallbackPath = "/signin-onegini";
+                c.AuthorizationEndpoint = "https://onegini-msp-snapshot.test.onegini.io/oauth/authorize";
+                c.TokenEndpoint = "https://onegini-msp-snapshot.test.onegini.io/oauth/token";
+                c.UserInformationEndpoint = "https://onegini-msp-snapshot.test.onegini.io/client/resource/profile";
+            });*/
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
